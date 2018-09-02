dfa80c814b4c14f0ab3803b0c258c46e2dc58063
Tit Petric <black@scene-si.org>
2018-09-02 13:46:16 +0200

    add global utilities for translation, formatting

--- crm/src/global.js
+++ crm/src/global.js
@@ -1,15 +1,43 @@
 import Vue from 'vue'
+import VTooltip from 'v-tooltip'
 
 // Global Axios defaults
 
 import axios from 'axios'
 axios.defaults.timeout = 15000
 
+// Global functions
+
+if (!String.format) {
+  String.format = function (format) {
+    var args = Array.prototype.slice.call(arguments, 1)
+    return format.replace(/{(\d+)}/g, function (match, number) {
+      return typeof args[number] !== 'undefined'
+        ? args[number]
+        : match
+    })
+  }
+}
+
 // Global Vue settings
 
 Vue.config.productionTip = false
 
 // Global Vue components
+
+Vue.use(VTooltip)
+
+Vue.mixin({
+  methods: {
+    // @todo: placeholder translation method with a "gettext" signature (underscore fn)
+    _: function (key, params) {
+      if (typeof params !== 'undefined') {
+        return String.format(key, ...params)
+      }
+      return key
+    }
+  }
+})
 
 addComponents([
   'Navigation',
