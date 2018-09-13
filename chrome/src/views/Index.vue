bf124fe535fcaba96e30b42830a1ad01f4473e25
Tit Petric <black@scene-si.org>
2018-09-13 15:29:18 +0200

    migrate assets from sam app

---
+++ chrome/src/views/Index.vue
@@ -0,0 +1,3 @@
+<template>
+  <p>This is the index</p>
+</template>
---
+++ chrome/vue.config.js
@@ -0,0 +1,12 @@
+module.exports = {
+  configureWebpack: {
+    // other webpack options to merge in ...
+  },
+  // devServer Options don't belong into `configureWebpack`
+  devServer: {
+    host: '0.0.0.0',
+    hot: true,
+    disableHostCheck: true,
+  },
+  runtimeCompiler: true,
+}
