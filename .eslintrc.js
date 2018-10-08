362042facd9d42acbb0c503006fbfe76e9471564
Antoine Souesme <antoine.souesme@needone.fr>
2018-10-08 10:01:54 +0200

    chore(linter): Added Linter, Prettier

--- .eslintrc.js
+++ .eslintrc.js
@@ -1,18 +1,19 @@
 module.exports = {
-  root: true,
-  env: {
-    node: true,
-  },
-  'extends': [
-    'plugin:vue/essential',
-    '@vue/standard',
-  ],
-  rules: {
-    'no-console': process.env.NODE_ENV === 'production' ? 'error' : 'off',
-    'no-debugger': process.env.NODE_ENV === 'production' ? 'error' : 'off',
-    'comma-dangle': ['error', 'always-multiline'],
-  },
-  parserOptions: {
-    parser: 'babel-eslint',
-  },
+    root: true,
+    env: {
+        node: true,
+    },
+    extends: [
+        'plugin:vue/essential',
+        // '@vue/standard',
+        '@vue/prettier',
+    ],
+    rules: {
+        'no-console': process.env.NODE_ENV === 'production' ? 'error' : 'off',
+        'no-debugger': process.env.NODE_ENV === 'production' ? 'error' : 'off',
+        'comma-dangle': ['error', 'always-multiline'],
+    },
+    parserOptions: {
+        parser: 'babel-eslint',
+    },
 }
--- src/views/Builder.vue
+++ src/views/Builder.vue
@@ -1,66 +1,66 @@
 <template>
-  <div class="container" v-if="loaded">
-    <!-- template v-if="error">
+    <div class="container" v-if="loaded">
+        <!-- template v-if="error">
       <p>An error occured: {{error}}</p>
     </template -->
-    <template v-if="modules.length == 0">
-      <p>No modules have been created yet.</p>
-      <a class="btn btn-primary" href="/modules/edit">Create new module</a>
-    </template>
-    <ul v-else>
-      <li v-for="module in modules" :key="module.id">
-        <a :href="module.links.read">{{module.name}}</a> [<a :href="module.links.edit">edit</a>]
-      </li>
-    </ul>
-  </div>
+        <template v-if="modules.length == 0">
+            <p>No modules have been created yet.</p>
+            <a class="btn btn-primary" href="/modules/edit">Create new module</a>
+        </template>
+        <ul v-else>
+            <li v-for="module in modules" :key="module.id">
+                <a :href="module.links.read">{{module.name}}</a> [<a :href="module.links.edit">edit</a>]
+            </li>
+        </ul>
+    </div>
 </template>
 
 <script>
 import crm from '@/client/crm'
 
 export default {
-  data () {
-    return {
-      'loaded': true,
-      'modules': [],
-      'result': {},
-    }
-  },
-
-  created () {
-    this.moduleList()
-  },
-
-  methods: {
-    moduleList () {
-      this.clearError()
-      var moduleList = (response) => {
-        if ('error' in response.data) {
-          this.showError(response.data.error.message)
-          return
+    data() {
+        return {
+            loaded: true,
+            modules: [],
+            result: {},
         }
-        if (Array.isArray(response.data.response)) {
-          this.modules.splice(0)
-          response.data.response.forEach((module) => {
-            module.links = {
-              'read': '/modules/' + module.id,
-              'edit': '/modules/' + module.id + '/edit',
-            }
-            this.modules.push(module)
-          })
-          return
-        }
-        this.showError('Unexpected response when fetching module list')
-      }
-      var moduleListFinalizer = () => {
-        this.loaded = true
-      }
-
-      crm.moduleList()
-        .then(moduleList)
-        .catch((e) => this.showError(e))
-        .finally(moduleListFinalizer)
     },
-  },
+
+    created() {
+        this.moduleList()
+    },
+
+    methods: {
+        moduleList() {
+            this.clearError()
+            var moduleList = response => {
+                if ('error' in response.data) {
+                    this.showError(response.data.error.message)
+                    return
+                }
+                if (Array.isArray(response.data.response)) {
+                    this.modules.splice(0)
+                    response.data.response.forEach(module => {
+                        module.links = {
+                            read: '/modules/' + module.id,
+                            edit: '/modules/' + module.id + '/edit',
+                        }
+                        this.modules.push(module)
+                    })
+                    return
+                }
+                this.showError('Unexpected response when fetching module list')
+            }
+            var moduleListFinalizer = () => {
+                this.loaded = true
+            }
+
+            crm.moduleList()
+                .then(moduleList)
+                .catch(e => this.showError(e))
+                .finally(moduleListFinalizer)
+        },
+    },
 }
 </script>
