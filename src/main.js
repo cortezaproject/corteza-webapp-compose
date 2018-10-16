c98e5ce2a5bc355e2d387267606e50babda9e8a6
Antoine Souesme <antoine.souesme@needone.fr>
2018-10-16 11:40:59 +0200

    Added config.js existence check

--- src/main.js
+++ src/main.js
@@ -2,18 +2,20 @@
 // (runtime-only or standalone) has been set in webpack.base.conf with an alias.
 import Vue from 'vue'
 import App from './App'
-
 import './global'
 import './plugins'
-
 import router from './router'
 import store from './store'
 
-/* eslint-disable no-new */
-new Vue({
-  el: '#app',
-  store,
-  router,
-  template: '<App/>',
-  components: { App },
-})
+if (window.CrustConfig === undefined) {
+  alert('Unexisting or invalid configuration. Make sure there is a public/config.js configuration file.')
+} else {
+  /* eslint-disable no-new */
+  new Vue({
+    el: '#app',
+    store,
+    router,
+    template: '<App/>',
+    components: { App },
+  })
+}
