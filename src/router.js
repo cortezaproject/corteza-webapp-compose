3acc90ef7daecb97304ccc1df84a826eb59e9f6d
Florent Pasquer <florent@needone.fr>
2018-10-16 14:38:30 +0200

    lint

--- src/router.js
+++ src/router.js
@@ -1,6 +1,6 @@
-import store from '@/store';
-import Vue from 'vue';
-import VueRouter from 'vue-router';
+import store from '@/store'
+import Vue from 'vue'
+import VueRouter from 'vue-router'
 
 Vue.use(VueRouter)
 
--- src/store/modules/pages.js
+++ src/store/modules/pages.js
@@ -1,6 +1,6 @@
 'use strict'
 
-import Vue from 'vue';
+import Vue from 'vue'
 
 const state = {
   initListError: '',
