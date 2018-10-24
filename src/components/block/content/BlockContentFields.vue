31c5d2faa77548e6eeda5d46ec460040c18c8908
Florent Pasquer <florent@needone.fr>
2018-10-24 10:15:10 +0200

    remove form input for builder

--- src/components/block/content/BlockContentFields.vue
+++ src/components/block/content/BlockContentFields.vue
@@ -1,60 +1,61 @@
 <template>
   <div class="block-content-fields">
-    <form>
-      <fieldset>
-        <div v-for="field in sortedFields" :key="field.id">
-          <div v-if="field.kind === 'text'">
-            <label class="">{{field.name}}</label>
-            <input type="text" class="">
-          </div>
-          <div v-else-if="field.kind === 'textarea'">
-            <label class="">{{field.name}}</label>
-            <textarea></textarea>
-          </div>
-          <div v-else-if="field.kind === 'bool'">
-            <label class="">{{field.name}}</label>
-            <input type="checkbox">
-          </div>
-          <div v-else-if="field.kind === 'email'">
-            <label class="">{{field.name}}</label>
-            <input type="email">
-          </div>
-          <div v-else-if="field.kind === 'stamp'">
-            <label class="">{{field.name}}</label>
-            <input type="datetime">
-          </div>
-          <div v-else-if="field.kind === 'enum'">
-            <label class="">{{field.name}}</label>
-            <select>
-              <option>TODO</option>
-            </select>
-          </div>
-          <div v-else>
-            Unknow kind od field.
-          </div>
+    <form class="">
+      <div v-for="field in sortedFields" :key="field.id">
+        <div class="form-group" v-if="field.kind === 'text'">
+          <label>{{field.name}}</label>
+          <!-- <input class="form-control" type="text"> -->
         </div>
-      </fieldset>
-      <fieldset class="form-group">
-        <!-- <button disabled type="submit" class="btn btn-primary">Submit</button> -->
-      </fieldset>
+        <div class="form-group" v-else-if="field.kind === 'textarea'">
+          <label>{{field.name}}</label>
+          <!-- <textarea class="form-control"></textarea> -->
+        </div>
+        <!-- <div class="form-group form-check" v-else-if="field.kind === 'bool'">
+          <input type="checkbox" id="visible" class="form-check-input">
+          <label for="visible" class="form-check-label">{{field.name}}</label>
+        </div> -->
+        <div class="form-group" v-else-if="field.kind === 'bool'">
+          <label for="visible" class="form-check-label">{{field.name}}</label>
+        </div>
+        <div class="form-group" v-else-if="field.kind === 'email'">
+          <label>{{field.name}}</label>
+          <!-- <input class="form-control" type="email"> -->
+        </div>
+        <div class="form-group" v-else-if="field.kind === 'stamp'">
+          <label>{{field.name}}</label>
+          <!-- <input class="form-control" type="datetime"> -->
+        </div>
+        <div class="form-group" v-else-if="field.kind === 'enum'">
+          <label>{{field.name}}</label>
+          <!-- <select class="form-control">
+            <option>TODO</option>
+          </select> -->
+        </div>
+        <div v-else>
+          Unknow kind od field.
+        </div>
+      </div>
+      <!-- <button disabled type="submit" class="btn btn-primary">Submit</button> -->
     </form>
   </div>
 </template>
 
 <script>
 export default {
-  name: 'BlockContentFields',
-  props: ['fields'],
+  name: "BlockContentFields",
+  props: ["fields"],
   computed: {
-    sortedFields () {
+    sortedFields() {
       // TODO Sort ?
-      return this.fields.concat()
-    },
-  },
-}
+      return this.fields.concat();
+    }
+  }
+};
 </script>
 <style lang="scss" scoped>
-fieldset {
-  border: none;
+@import "~bootstrap/scss/bootstrap";
+.block-content-fields /deep/ {
+  // Bootstrap Modal doesnt work if we comment this line
+  @import "~bootstrap/scss/bootstrap";
 }
 </style>
