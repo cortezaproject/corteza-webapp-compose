295ca7559f47e23d8342059e0d42122d2922ddba
Florent Pasquer <florent@needone.fr>
2018-10-24 09:53:31 +0200

    lint

--- src/views/Public/Pages/View.vue
+++ src/views/Public/Pages/View.vue
@@ -1,12 +1,21 @@
 <template>
-  <div>
-    <nav class="menu navbar navbar-expand-lg navbar-light bg-light">
-      <ul class="navbar-nav mr-auto">
-        <li class="nav-item" v-bind:class="{ active: page.id === viewPageData.id }" v-for="page in pages" v-bind:key="page.id">
-          <router-link class="nav-link" :to="'/pages/' + page.id">{{page.title}}</router-link>
-        </li>
-      </ul>
-    </nav>
+  <div class="view">
+    <b-navbar toggleable="md" type="dark" variant="info">
+      <b-navbar-toggle target="nav_collapse"></b-navbar-toggle>
+      <b-navbar-brand>CRUST CRM</b-navbar-brand>
+      <b-collapse is-nav id="nav_collapse">
+        <b-navbar-nav>
+          <b-nav-item-dropdown :title="page.description" :text="page.title" v-if="page.children.length > 0" v-bind:class="{ 'active': page.active }" v-for="page in pages" v-bind:key="page.id">
+            <b-dropdown-item :title="page.description" v-bind:class="{ 'active': page.active && viewPageData.id === page.id }"  :to="'/pages/' + page.id">{{ page.title }}</b-dropdown-item>
+            <b-dropdown-item :title="pageChild.description" v-bind:class="{ 'active': pageChild.active }"  v-for="pageChild in page.children" v-bind:key="pageChild.id" :to="'/pages/' + pageChild.id">{{ pageChild.title }}</b-dropdown-item>
+          </b-nav-item-dropdown>
+          <b-nav-item :title="page.description"  v-if="page.children.length == 0" :to="'/pages/' + page.id" v-bind:class="{ 'active': page.active }" v-for="page in pages" v-bind:key="page.id">
+            {{page.title}}
+          </b-nav-item>
+        </b-navbar-nav>
+      </b-collapse>
+    </b-navbar>
+
     <div class="content">
       <PageContent></PageContent>
     </div>
@@ -48,7 +57,50 @@ export default {
   },
   computed: {
     ...mapState({
-      pages: state => state.pages.list.filter(page => !page.module),
+      pages: state => {
+        // Parent page : filter by page with no module + visible + no parent
+        const parentPages = state.pages.list.filter(page => {
+          return (
+            page.visible &&
+            !page.module &&
+            (page.selfID === 0 ||
+              page.selfID === '0' ||
+              page.selfID === null ||
+              page.selfID === undefined)
+          )
+        })
+        // Children page : filter by no module + visible + has parent
+        const childrenPages = state.pages.list.filter(page => {
+          return (
+            page.visible &&
+            !page.module &&
+            (page.selfID !== 0 &&
+              page.selfID !== '0' &&
+              page.selfID !== null &&
+              page.selfID !== undefined)
+          )
+        })
+
+        // Add chidren to each parent + set the 'active' property
+        parentPages.forEach(value => {
+          value.children = []
+          value.active = value.id === state.pages.viewPageData.id
+          childrenPages.forEach(valueHere => {
+            valueHere.active = valueHere.id === state.pages.viewPageData.id
+
+            // If this child has this parent
+            if (valueHere.selfID === value.id) {
+              value.children.push(valueHere)
+
+              // If child active => parent too !
+              if (valueHere.id === state.pages.viewPageData.id) {
+                value.active = true
+              }
+            }
+          })
+        })
+        return parentPages
+      },
       viewPageData: state => state.pages.viewPageData,
     }),
   },
@@ -57,19 +109,9 @@ export default {
 <style lang="scss" scoped>
 @import "src/assets/styles/shared";
 @import "~bootstrap/scss/bootstrap";
-
-.menu {
-  display: flex;
-  flex-direction: row;
-  justify-content: flex-start;
-  width: 100%;
-  padding: 25px;
-  a {
-    &.active {
-      text-decoration: none;
-    }
-    margin: 5px;
-  }
+.view /deep/ {
+  // Bootstrap Modal doesnt work if we comment this line
+  @import "~bootstrap/scss/bootstrap";
 }
 .content {
 }
