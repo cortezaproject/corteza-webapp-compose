b38d15695e4e199aff6de5917c5b4c935d445963
Tit Petric <black@scene-si.org>
2018-11-06 14:15:52 +0100

    import FieldEdit.vue, fix

---
+++ src/components/FieldEdit.vue
@@ -0,0 +1,11 @@
+<template>
+  <p>Field {{field.name}}, type {{field.kind}}, components/Field/{{field.kind.charAt(0).toUpperCase() + field.kind.substring(1)}}Edit.vue</p>
+</template>
+
+<script>
+export default {
+  props: {
+    field: Object,
+  },
+}
+</script>
