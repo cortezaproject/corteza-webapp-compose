728b83ca82c26ba4bbbc621139fc3c17ddce31f3
Tit Petric <black@scene-si.org>
2018-11-15 11:43:11 +0100

    implement text edit field

---
+++ src/components/Field/TextEdit.vue
@@ -0,0 +1,16 @@
+<template>
+  <div class="form-group">
+    <!-- <label :for="field.name">{{field.title}}</label> -->
+    <input type="text" v-model="row[field.name]" class="form-control" />
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'FieldTextEdit',
+  props: {
+    row: Object,
+    field: Object,
+  },
+}
+</script>
--- src/components/FieldEdit.vue
+++ src/components/FieldEdit.vue
@@ -1,5 +1,5 @@
 <template>
-  <p>Field {{field.name}}, type {{field.kind}}, components/Field/{{field.kind.charAt(0).toUpperCase() + field.kind.substring(1)}}Edit.vue</p>
+  <p>Field {{field.name}}, type {{field.kind}}, components/Field/{{field.kind.charAt(0).toUpperCase() + field.kind.substring(1)}}Edit.vue<br/>{{field}}</p>
 </template>
 
 <script>
