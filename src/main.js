09b5f0370ded38ed6817b283fb4e3c479407fc0a
Tit Petric <black@scene-si.org>
2018-11-16 15:18:20 +0100

    add logger, print crust version and build time

--- src/main.js
+++ src/main.js
@@ -9,6 +9,13 @@ import './main.scss'
 
 import router from './router'
 import store from './store'
+import logger from './logger'
+
+/* eslint-disable no-undef */
+logger.log(
+  `%cCrust CRM, version: ${CRUST_VERSION}, build time: ${CRUST_BUILD_TIME}`,
+  'background-color: #1397CB; color: white; padding: 3px 10px; border: 1px solid black; font: Courier',
+)
 
 if (window.CrustConfig === undefined) {
   alert('Unexisting or invalid configuration. Make sure there is a public/config.js configuration file.')
