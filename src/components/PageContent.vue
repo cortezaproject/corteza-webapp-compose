2acefbb3d7ba69398b2ddb64b5a78d9a1c9d0712
i-am-dev <i-am-dev@users.noreply.github.com>
2018-11-20 21:45:57 +0200

    The documentation for the vue-grid-layout component is incorrect in that the breakpoints and col sizes for each breakpoint has to be specified

--- src/components/PageContent.vue
+++ src/components/PageContent.vue
@@ -1,11 +1,21 @@
 <template>
   <div class="view-grid">
-    <grid-layout v-if="layout && layout.length > 0" :layout.sync="layout" :col-num="colNum" :row-height="90" :is-draggable="false" :is-resizable="false" :vertical-compact="true" :use-css-transforms="true" :responsive="responsive">
-      <grid-item v-for="block in layout" v-bind:key="block.i" :x="block.x" :y="block.y" :w="block.w" :h="block.h" :i="block.i" v-bind:is-draggable="false">
+    <grid-layout v-if="internalLayout && internalLayout.length > 0"
+      :layout.sync="internalLayout"
+      :col-num="colNum"
+      :row-height="90"
+      :is-draggable="false"
+      :is-resizable="false"
+      :vertical-compact="true"
+      :use-css-transforms="true"
+      :responsive="responsive"
+      :breakpoints="gridBreakPoints"
+      :cols="gridColSizes">
+      <grid-item v-for="block in internalLayout" v-bind:key="block.i" :x="block.x" :y="block.y" :w="block.w" :h="block.h" :i="block.i" v-bind:is-draggable="false">
         <Block :block="block"></Block>
       </grid-item>
     </grid-layout>
-    <div style="color:red;" v-if="!layout || layout.length == 0">
+    <div style="color:red;" v-if="!internalLayout || internalLayout.length == 0">
       This page has no blocks
     </div>
   </div>
@@ -13,7 +23,7 @@
 
 <script>
 /* eslint-disable-next-line */
-import VueGridLayout from "vue-grid-layout";
+import VueGridLayout from 'vue-grid-layout'
 import Block from '@/components/block/Block'
 export default {
   name: 'PageContent',
@@ -21,7 +31,7 @@ export default {
     Block,
   },
   props: {
-    layout: [],
+    layout: Array,
   },
   data () {
     return {
@@ -31,7 +41,34 @@ export default {
       },
       colNum: 12,
       blocks: null,
+      gridBreakPoints: {
+        lg: 1200,
+        md: 996,
+        sm: 768,
+        xs: 480,
+        xxs: 0,
+      },
+      gridColSizes: {
+        lg: 12,
+        md: 10,
+        sm: 6,
+        xs: 4,
+        xxs: 2,
+      },
+      responsive: true,
+      internalLayout: [],
     }
+  },
+  created () {
+    this.internalLayout = this.layout
+  },
+  watch: {
+    layout: {
+      handler: function () {
+        this.internalLayout = this.layout
+      },
+      deep: true,
+    },
   },
 }
 </script>
