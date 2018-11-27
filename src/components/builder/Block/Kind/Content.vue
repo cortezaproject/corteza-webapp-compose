e39e8f57b4d903356d7c175070a5d965f8e1f664
Denis Arh <denis.arh@kendu.si>
2018-11-27 17:52:52 +0100

    Quill for content editing

--- src/components/builder/Block/Kind/Content.vue
+++ src/components/builder/Block/Kind/Content.vue
@@ -1,13 +1,18 @@
 <template>
     <div>
         <fieldset class="form-group">
-            @todo A text block that allows HTML. Best would be a TinyMCE or similar.
-            <textarea rows="5" v-model="o.body"></textarea>
+            <quill-editor v-model="o.body" :options="quillOptions"></quill-editor>
         </fieldset>
     </div>
 </template>
 <script>
 import { Content } from '@/lib/block/Content'
+import { quillEditor } from 'vue-quill-editor'
+
+// Quill styles
+import 'quill/dist/quill.core.css'
+import 'quill/dist/quill.snow.css'
+// import 'quill/dist/quill.bubble.css'
 
 export default {
   props: {
@@ -27,6 +32,49 @@ export default {
         this.$emit('update:options', o)
       },
     },
+  },
+
+  data () {
+    return {
+      quillOptions: {
+        modules: {
+          toolbar: [
+            // toggled buttons
+            ['bold', 'italic', 'underline', 'strike'],
+            ['blockquote', 'code-block'],
+
+            // custom button values
+            [{ 'header': 1 }, { 'header': 2 }],
+            [{ 'list': 'ordered' }, { 'list': 'bullet' }],
+
+            // superscript/subscript
+            // [{ 'script': 'sub' }, { 'script': 'super' }],
+
+            // outdent/indent
+            [{ 'indent': '-1' }, { 'indent': '+1' }],
+
+            // text direction
+            // [{ 'direction': 'rtl' }],
+
+            // custom dropdown
+            // [{'size': ['small', false, 'large', 'huge']}],
+            [{ 'header': [1, 2, 3, 4, 5, 6, false] }],
+
+            // dropdown with defaults from theme
+            // [{ 'color': [] }, { 'background': [] }],
+            // [{ 'font': [] }],
+            [{ 'align': [] }],
+
+            // remove formatting button
+            ['clean'],
+          ],
+        },
+      },
+    }
+  },
+
+  components: {
+    quillEditor,
   },
 }
 </script>
