7e835b54238075759303403dd0c78bea6045c51f
i-am-dev <i-am-dev@users.noreply.github.com>
2018-11-29 23:36:38 +0200

    Created component for each field in record view

---
+++ src/lib/block/View/fields/Email.vue
@@ -0,0 +1,16 @@
+<template>
+  <div class="form-group">
+    <label>{{field.name}}</label>
+    <input class="form-control" type="email">
+  </div>
+</template>
+<script>
+import fieldPropMixin from './mixins/fieldProp'
+
+export default {
+  name: 'fieldemail',
+  computed: {},
+
+  mixins: [fieldPropMixin],
+}
+</script>
---
+++ src/lib/block/View/fields/Enum.vue
@@ -0,0 +1,18 @@
+<template>
+  <div class="form-group">
+    <label>{{field.name}}</label>
+    <select class="form-control">
+      <option>TODO</option>
+    </select>
+  </div>
+</template>
+<script>
+import fieldPropMixin from './mixins/fieldProp'
+
+export default {
+  name: 'fieldenum',
+  computed: {},
+
+  mixins: [fieldPropMixin],
+}
+</script>
