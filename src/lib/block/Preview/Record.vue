2d1da320e10af75a6a0574afb3362a4913d9111e
i-am-dev <i-am-dev@users.noreply.github.com>
2018-12-02 18:00:03 +0200

    Implemented rendering of fields on page builder

--- src/lib/block/Preview/Record.vue
+++ src/lib/block/Preview/Record.vue
@@ -30,7 +30,7 @@ export default {
   ],
 
   components: {
-    ... FieldTypes,
+    ...FieldTypes,
   },
 }
 </script>
--- src/lib/block/View/Record.vue
+++ src/lib/block/View/Record.vue
@@ -1,41 +1,7 @@
 <template>
-  <div v-if="record">
+  <div v-if="options">
     <div v-for="field in options.fields" :key="field.id">
       <FieldTypes v-bind:is="mapFieldKind(field.kind)" :field.sync="field"></FieldTypes>
-
-      <p>TEST</p>
-
-      <!-- <div class="form-group" v-if="field.kind === 'text'">-->
-        <!-- <label>{{field.name}}</label>-->
-        <!-- <input class="form-control" type="text"> -->
-      <!-- </div>-->
-      <!-- <div class="form-group" v-else-if="field.kind === 'textarea'">-->
-        <!-- <label>{{field.name}}</label>-->
-        <!-- <textarea class="form-control"></textarea> -->
-      <!-- </div>-->
-      <!-- <div class="form-group form-check" v-else-if="field.kind === 'bool'">
-          <input type="checkbox" id="visible" class="form-check-input">
-          <label for="visible" class="form-check-label">{{field.name}}</label>
-      </div>-->
-      <!-- <div class="form-group" v-else-if="field.kind === 'bool'">-->
-        <!-- <label class="form-check-label">{{field.name}}</label>
-      </div>
-      <div class="form-group" v-else-if="field.kind === 'email'">
-        <label>{{field.name}}</label>-->
-        <!-- <input class="form-control" type="email"> -->
-      <!-- </div>
-      <div class="form-group" v-else-if="field.kind === 'stamp'">
-        <label>{{field.name}}</label>-->
-        <!-- <input class="form-control" type="datetime"> -->
-      <!-- </div>
-      <div class="form-group" v-else-if="field.kind === 'enum'">
-        <label>{{field.name}}</label>-->
-        <!-- <select class="form-control">
-            <option>TODO</option>
-        </select>-->
-      <!-- </div>
-      <div v-else>Unknow kind od field.</div>
-      {{ recordValue(field) }}-->
     </div>
   </div>
   <div v-else>Can not render this block without a record</div>
@@ -68,7 +34,7 @@ export default {
   mixins: [optionsPropMixin],
 
   components: {
-    ... FieldTypes,
+    ...FieldTypes,
   },
 }
 </script>
