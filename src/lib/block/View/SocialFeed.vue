c2543a5d6f38cfdb4824ad447d2d8dde80a64220
Richard <i-am-dev@users.noreply.github.com>
2018-12-04 22:14:51 +0200

    Added Twitter to social block. Needs final wiring up upon confirmation of what the JSON response object looks like from the API in "options"

--- src/lib/block/View/SocialFeed.vue
+++ src/lib/block/View/SocialFeed.vue
@@ -1,14 +1,29 @@
 <template>
     <div>
-        {{ options }}
+      <Timeline :id="this.options.twitter" :sourceType="'profile'" :options="{ tweetLimit: '3' }">
+        <div class="spinner"></div>
+      </Timeline>
     </div>
 </template>
 <script>
 import optionsPropMixin from './mixins/optionsProp'
+import Timeline from 'vue-tweet-embed/timeline'
+
+/// ///////////////////////////////////
+/// INFO: Custom class to Twitter timeline
+/// add :widget-class="`my-custom-class-1 my-custom-class-2`"
+/// to the <Timeline/> element
+/// ///////////////////////////////////
 
 export default {
   mixins: [
     optionsPropMixin,
   ],
+  components: {
+    Timeline,
+  },
+  created () {
+    this.options.twitter = 'zorosa'
+  },
 }
 </script>
