11ce1e2c3a80504605c2a137fd3b6164128a8e5a
Richard <i-am-dev@users.noreply.github.com>
2018-12-04 22:14:51 +0200

    Added Twitter to social block. Needs final wiring up upon confirmation of what the JSON response object looks like from the API in "options"

--- src/lib/block/View/SocialFeed.vue
+++ src/lib/block/View/SocialFeed.vue
@@ -1,12 +1,22 @@
 <template>
     <div>
-        {{ options }}
+      <Timeline :id="this.options.twitter" :sourceType="'profile'" :options="{ tweetLimit: '3' }">
+        <div class="spinner"></div>
+      </Timeline>
     </div>
 </template>
 <script>
 import base from './base'
+import Timeline from 'vue-tweet-embed/timeline'
 
 export default {
   extends: base,
+
+  components: {
+    Timeline,
+  },
+  created () {
+    this.options.twitter = 'zorosa'
+  },
 }
 </script>
