d5260c664b032c2fdc7d39c165ae3b56215cc6ef
Denis Arh <denis.arh@kendu.si>
2018-12-06 12:00:43 +0100

    Make record block look nicer

--- src/lib/block/View/Record.vue
+++ src/lib/block/View/Record.vue
@@ -1,10 +1,16 @@
 <template>
-  <div v-if="record && options" >
-    <field-viewer
-      v-for="(field, index) in fields"
-      :key="index"
-      :field="field"
-      :record="record || {}"></field-viewer>
+  <div v-if="record && options" class="record">
+    <div v-for="(field, index) in fields"
+         class="field"
+         :key="index">
+      <label>{{ field.label || field.name }}</label>
+      <div>
+        <field-viewer
+          :field="field"
+          value-only
+          :record="record || {}"></field-viewer>
+      </div>
+    </div>
   </div>
   <div v-else>Can not render this block without a record</div>
 </template>
@@ -40,3 +46,22 @@ export default {
   },
 }
 </script>
+<style scoped lang="scss">
+@import "@/assets/sass/_0.declare.scss";
+
+.record {
+  .field {
+    margin-bottom: 5px;
+    label {
+      color: $appgrey;
+      font-weight: normal;
+      width: 150px;
+      display: inline-block;
+    }
+    div {
+      min-width: 200px;
+      display: inline-block;
+    }
+  }
+}
+</style>
