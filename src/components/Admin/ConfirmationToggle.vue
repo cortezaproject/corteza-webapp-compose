f77d723d76d3fe8b0c8ad591d6fdadca54b0b5a3
Mia Arh <mia@kendu.si>
2018-12-06 17:25:29 +0100

    Fix confirmation toggle delete event

--- src/components/Admin/ConfirmationToggle.vue
+++ src/components/Admin/ConfirmationToggle.vue
@@ -1,11 +1,11 @@
 <template>
   <span>
     <span v-if="!inConfirmation">
-      <button class="btn" :class="btnClass" @click="inConfirmation=true" :disabled="disabled"><slot></slot></button>
+      <button type="button" class="btn" :class="btnClass" @click.prevent="inConfirmation=true" :disabled="disabled"><slot></slot></button>
     </span>
     <span v-if="inConfirmation">
-      <button class="btn" :class="confirmationClass" @click="onConfirmation()">Yes</button>
-      <button class="btn" @click="inConfirmation=false">Cancel</button>
+      <button type="button" class="btn" :class="confirmationClass" @click.prevent="onConfirmation()">Yes</button>
+      <button type="button" class="btn" @click.prevent="inConfirmation=false">Cancel</button>
     </span>
   </span>
 </template>
