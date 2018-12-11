5ae57789937a11a35638734f6161294f0e5159e6
Mia Arh <mia@kendu.si>
2018-12-11 17:34:07 +0100

    Add boolean settings for selected value

---
+++ src/lib/field/Configurator/Bool.vue
@@ -0,0 +1,20 @@
+<template>
+  <div>
+    <div>
+      <label class="d-block">Value when marked as true</label>
+      <b-form-input v-model="f.options.trueLabel" placeholder="True value"></b-form-input>
+    </div>
+    <div>
+      <label class="d-block">Value when marked as false</label>
+      <b-form-input v-model="f.options.falseLabel" placeholder="False value"></b-form-input>
+    </div>
+  </div>
+</template>
+
+<script>
+import base from './base'
+
+export default {
+  extends: base,
+}
+</script>
