c22af08658f9fcc303d6e652b3fe34bd4dc9841c
Mia Arh <mia@kendu.si>
2018-12-11 17:36:20 +0100

    Change placeholder and label text on boolean settings modal

--- src/lib/field/Configurator/Bool.vue
+++ src/lib/field/Configurator/Bool.vue
@@ -1,12 +1,12 @@
 <template>
   <div>
     <div>
-      <label class="d-block">Value when marked as true</label>
-      <b-form-input v-model="f.options.trueLabel" placeholder="True value"></b-form-input>
+      <label class="d-block">Value when checked</label>
+      <b-form-input v-model="f.options.trueLabel" placeholder="Checked value"></b-form-input>
     </div>
     <div>
-      <label class="d-block">Value when marked as false</label>
-      <b-form-input v-model="f.options.falseLabel" placeholder="False value"></b-form-input>
+      <label class="d-block">Value when unchecked</label>
+      <b-form-input v-model="f.options.falseLabel" placeholder="Unchecked value"></b-form-input>
     </div>
   </div>
 </template>
