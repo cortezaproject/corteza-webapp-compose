ce14ba2ec8f0e98b7635addc2dd0b667e8b94ccb
Richard <i-am-dev@users.noreply.github.com>
2018-12-11 23:39:16 +0200

    Finished String Field

--- src/lib/field/Editor/String.vue
+++ src/lib/field/Editor/String.vue
@@ -2,17 +2,35 @@
   <div class="form-group">
     {{ field.label || field.name }}
     <b-form-textarea
-      v-if="field.options.multiLine"
+      v-if="field.options.multiLine && !field.options.useRichTextEditor"
       v-model="value"></b-form-textarea>
+    <quill-editor v-model="value"
+                ref="myQuillEditor"
+                v-if="field.options.multiLine && field.options.useRichTextEditor"
+                :options="editorOption"
+                @blur="onEditorBlur($event)"
+                @focus="onEditorFocus($event)"
+                @ready="onEditorReady($event)">
+    </quill-editor>
     <b-form-input
-      v-else
+      v-if="!field.options.multiLine"
       v-model="value"></b-form-input>
   </div>
 </template>
 <script>
+// require styles
+import 'quill/dist/quill.core.css'
+import 'quill/dist/quill.snow.css'
+import 'quill/dist/quill.bubble.css'
+
+import { quillEditor } from 'vue-quill-editor'
+
 import base from './base'
 
 export default {
   extends: base,
+  components: {
+    quillEditor,
+  },
 }
 </script>
--- src/lib/field/Viewer/String.vue
+++ src/lib/field/Viewer/String.vue
@@ -1,3 +1,9 @@
+<template>
+  <div>
+    <div v-if="field.options.useRichTextEditor"><span v-html="formatted"></span></div>
+    <div v-else>{{ formatted }}</div>
+  </div>
+</template>
 <script>
 import base from './base'
 
