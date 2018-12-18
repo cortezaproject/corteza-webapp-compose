3b04129953386489199cc6f7b48bf2d438c90584
Richard <i-am-dev@users.noreply.github.com>
2018-12-18 23:45:10 +0200

    Completed DateTime for Edit. Added code to make sure certain DateTime options cannot be selected at the same time

--- src/lib/field/DateTime.js
+++ src/lib/field/DateTime.js
@@ -1,7 +1,52 @@
-// @todo option to allow only date entry
 // @todo option to allow only time entry
-// @todo option to allow only future entries
-// @todo option to allow only past entries
 // @todo option to allow multiple entries
 // @todo option to allow duplicates
-export class DateTime {}
+import moment from 'moment'
+
+export class DateTime {
+  constructor (def = {}) {
+    this.merge(def)
+  }
+
+  merge ({ onlyDate, onlyTime, onlyPastValues, onlyFutureValues, outputRelative } = {}) {
+    this.onlyDate = onlyDate || false
+    this.onlyTime = onlyTime || false
+    this.onlyPastValues = onlyPastValues || false
+    this.onlyFutureValues = onlyFutureValues || false
+    this.outputRelative = outputRelative || false
+
+    return this
+  }
+}
+
+export function checkPast (value, onlyPast, onlyDate, onlyTime) {
+  if (onlyPast) {
+    // make sure value is in the past
+    if (value < moment()) {
+      return value
+    }
+    return moment()
+  }
+}
+
+export function checkFuture (value, onlyFuture, onlyDate, onlyTime) {
+  if (onlyFuture) {
+    // make sure value is in the future
+    if (value > moment()) {
+      return value
+    }
+    return moment()
+  }
+}
+
+export function checkFuturePast (value, onlyFuture, onlyPast, onlyDate, onlyTime) {
+  // check future
+  if (onlyFuture) {
+    value = checkFuture(value, onlyFuture, onlyDate, onlyTime)
+  } else if (onlyPast) {
+    // check past
+    value = checkPast(value, onlyPast, onlyDate, onlyTime)
+  }
+
+  return value
+}
