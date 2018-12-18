ef4acd4561726864f24a6cf160aa87ed4d2fec5d
Richard <i-am-dev@users.noreply.github.com>
2018-12-18 23:45:10 +0200

    Completed DateTime for Edit. Added code to make sure certain DateTime options cannot be selected at the same time

---
+++ src/lib/field/Configurator/DateTime.vue
@@ -0,0 +1,17 @@
+<template>
+  <div>
+    <div><b-form-checkbox plain v-model="f.options.onlyDate" v-if="!f.options.onlyTime">Date Only</b-form-checkbox></div>
+    <div><b-form-checkbox plain v-model="f.options.onlyTime" v-if="!f.options.onlyDate">Time Only</b-form-checkbox></div>
+    <div><b-form-checkbox plain v-model="f.options.onlyPastValues" v-if="!f.options.onlyFutureValues">Past Values Only</b-form-checkbox></div>
+    <div><b-form-checkbox plain v-model="f.options.onlyFutureValues" v-if="!f.options.onlyPastValues">Future Value Only</b-form-checkbox></div>
+    <div><b-form-checkbox plain v-model="f.options.outputRelative">Make Output Relative</b-form-checkbox></div>
+  </div>
+</template>
+
+<script>
+import base from './base'
+
+export default {
+  extends: base,
+}
+</script>
