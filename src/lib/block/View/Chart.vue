b42cd4f2f2143ed61940d0dcbc61be27d81cff25
Denis Arh <denis.arh@kendu.si>
2019-01-11 19:58:41 +0100

    Safer DOM referecing

--- src/lib/block/View/Chart.vue
+++ src/lib/block/View/Chart.vue
@@ -20,24 +20,30 @@ export default {
     this.$crm.chartRead({ chartID: this.options.chartID }).then((chart) => {
       this.chart = new Chart(chart)
 
+      if (!this.chart.isValid()) {
+        return
+      }
+
       const opt = this.chart.buildOptions()
 
       if (!opt) {
         return
       }
 
-      this.renderer = new ChartJS(this.$refs.chartCanvas.getContext('2d'), opt)
+      this.$nextTick(() => {
+        this.renderer = new ChartJS(this.$refs.chartCanvas.getContext('2d'), opt)
 
-      this.chart.fetchReports({ reporter: (r) => this.$crm.moduleRecordReport(r) }).then(({ labels, metrics }) => {
-        if (labels) {
-          this.renderer.data.labels = labels
-        }
+        this.chart.fetchReports({ reporter: (r) => this.$crm.moduleRecordReport(r) }).then(({ labels, metrics }) => {
+          if (labels) {
+            this.renderer.data.labels = labels
+          }
 
-        metrics.forEach((metric, index) => {
-          this.renderer.data.datasets[index].data = metric
+          metrics.forEach((metric, index) => {
+            this.renderer.data.datasets[index].data = metric
+          })
+
+          this.renderer.update()
         })
-
-        this.renderer.update()
       })
     }).catch(this.defaultErrorHandler('Could not load chart'))
   },
