171363f34aa2412b1a2a5acf3d8228b458ad0fed
Tomaž Jerman <tomaz.jerman@kendu.si>
2019-02-20 13:17:24 +0100

    Collapse overflown nav elements under more

--- src/components/Public/Header.vue
+++ src/components/Public/Header.vue
@@ -8,7 +8,8 @@
             </label>
           </b-navbar-toggle>
           <b-collapse is-nav id="nav_text_collapse" :class="{ visible }" @show="toggleNav(true)" @hide="toggleNav(false)">
-            <menu-level :pages="pages"
+            <menu-level id="menu_lvl_1"
+                        :pages="pages"
                         :selectedPath="selectedPath"
                         :currentPageID="currentPageID"></menu-level>
           </b-collapse>
@@ -20,6 +21,8 @@
 <script>
 import { mapGetters } from 'vuex'
 import MenuLevel from './MenuLevel'
+
+const UNCOLLAPSE_BUFFER = 100
 
 export default {
   props: {
@@ -76,8 +79,57 @@ export default {
     },
   },
 
+  methods: {
+    collapser (nav, bb) {
+      const { children: nChildren = new HTMLCollection() } = nav
+      const collapse = nChildren[nChildren.length - 1]
+      const collapseBody = collapse.getElementsByTagName('UL')[0]
+      if (!collapseBody) {
+        return
+      }
+
+      const extraOffset = collapse.clientWidth || 0
+
+      // Check if overflow possible
+      if (nav.clientWidth === bb.clientWidth) {
+        let c = null
+        // -2; skip last element (the 'more' dropdown)
+        for (let i = nChildren.length - 2; i >= 0; i--) {
+          c = nChildren.item(i)
+
+          const { clientWidth, offsetLeft } = c
+          if (clientWidth + offsetLeft + extraOffset > bb.clientWidth) {
+            c.dataset.collapsed = true
+            collapseBody.appendChild(c)
+          }
+        }
+      } else {
+        // Check if any can be uncollapsed
+        const collapsedNodes = document.querySelectorAll('[data-collapsed]')
+
+        for (let i = collapsedNodes.length - 1; i >= 0; i--) {
+          const cn = collapsedNodes[i]
+          if (cn.clientWidth + nav.clientWidth + extraOffset + UNCOLLAPSE_BUFFER <= bb.clientWidth) {
+            delete cn.dataset.collapsed
+            nav.insertBefore(cn, collapse)
+          }
+        }
+      }
+    },
+  },
+
   created () {
     this.$crm.pageTree().then((result) => { this.tree = result })
+
+    this.$nextTick(() => {
+      const nav = document.getElementById('menu_lvl_1')
+      const bb = document.getElementById('nav_text_collapse')
+
+      this.collapser(nav, bb)
+      window.onresize = () => {
+        this.collapser(nav, bb)
+      }
+    })
   },
 
   components: {
