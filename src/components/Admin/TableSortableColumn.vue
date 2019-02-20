4e48da868c3541ecba69fcddc4c184bf2e404ab3
Jože Fortun <joze.fortun@gmail.com>
2019-02-20 20:50:11 +0100

    Add sortable table column component

---
+++ src/components/Admin/TableSortableColumn.vue
@@ -0,0 +1,53 @@
+<template>
+  <th @click="sort(!ascending)">
+    {{ label || name }}
+    <span v-if="!sortDisabled">
+      <font-awesome-icon v-if="ascending === undefined" :icon="['fas', 'sort']"></font-awesome-icon>
+      <font-awesome-icon v-else-if="ascending" :icon="['fas', 'sort-up']"></font-awesome-icon>
+      <font-awesome-icon v-else-if="!ascending" :icon="['fas', 'sort-down']"></font-awesome-icon>
+    </span>
+  </th>
+</template>
+
+<script>
+export default {
+  methods: {
+    sort (asc) {
+      const fieldObject = {
+        name: this.name,
+        ascending: asc,
+      }
+      this.$emit('sort', fieldObject)
+    },
+  },
+
+  props: {
+    label: {
+      type: String,
+      required: true,
+    },
+    name: {
+      type: String,
+      required: true,
+    },
+    ascending: {
+      type: Boolean,
+    },
+
+    sortDisabled: {
+      type: Boolean,
+      default: false,
+    },
+  },
+
+  created () {
+    if (this.ascending !== undefined) this.sort(this.ascending)
+  },
+}
+</script>
+
+<style scoped>
+th {
+  padding-left: 5px;
+}
+</style>
