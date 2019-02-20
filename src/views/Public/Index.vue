a6175bcdba208ed51d317355b68752916acc9cdd
Tomaž Jerman <tomaz.jerman@kendu.si>
2019-02-20 15:55:24 +0100

    Make mobile nav float on small screens

--- src/views/Public/Index.vue
+++ src/views/Public/Index.vue
@@ -1,19 +1,29 @@
 <template>
   <div class="centering-wrap inactive-area">
     <public-header :currentPageID="pageID" @toggleNav="navVisible = $event"></public-header>
-    <router-view :class="`crm-content ${navVisible ? 'padded' : ''}`" />
+    <router-view :class="`crm-content ${navVisible && canPushContent ? 'padded' : ''}`" />
   </div>
 </template>
 
 <script>
 import PublicHeader from '@/components/Public/Header'
+
+const pushContentAbove = 610
+
 export default {
   name: 'public-root',
 
   data () {
     return {
       navVisible: false,
+      documentWidth: 0,
     }
+  },
+
+  computed: {
+    canPushContent () {
+      return this.documentWidth > pushContentAbove
+    },
   },
 
   props: {
@@ -21,6 +31,13 @@ export default {
       type: String,
       required: true,
     },
+  },
+
+  created () {
+    this.documentWidth = document.body.offsetWidth
+    window.onresize = () => {
+      this.documentWidth = document.body.offsetWidth
+    }
   },
 
   components: {
