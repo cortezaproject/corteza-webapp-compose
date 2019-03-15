6e6b9cf4d784738dae546f152fe7ad68bfbd2e32
Tomaž Jerman <tomaz.jerman@kendu.si>
2019-03-15 07:36:50 +0100

    Update i18next config

--- src/i18next.js
+++ src/i18next.js
@@ -4,15 +4,13 @@ import i18next from 'i18next'
 import VueI18Next from '@panter/vue-i18next'
 
 // Plugins for i18next
-import cBackend from 'i18next-chained-backend'
 import xhrBackend from 'i18next-xhr-backend'
-// import localBackend from 'i18next-localstorage-backend'
 import lngDetector from 'i18next-browser-languagedetector'
 
 Vue.use(VueI18Next)
 
 i18next
-  .use(cBackend)
+  .use(xhrBackend)
   .use(lngDetector)
   .init({
     fallbackLng: window.CrustConfig.webapp.defaultLocale || 'en',
@@ -23,25 +21,7 @@ i18next
       caches: [/* 'localStorage', 'cookie' */],
     },
     backend: {
-      backends: [
-        // localBackend,
-        xhrBackend,
-      ],
-      backendOptions: [
-        // {
-        //   // Use session storage so we won't cache them for too long.
-        //   // Could use window.localStorage with a set expirationTime for eg. 1day or so
-        //   store: window.sessionStorage,
-
-        //   // Another option would be to use versions for languages, so we would only refetch
-        //   // if the version has changed; here ttl has to be a lot longer.
-        //   // https://github.com/i18next/i18next-localstorage-backend#cache-backend-options
-        // },
-        {
-          loadPath: `/lang/{{lng}}.json`,
-          crossDomain: false,
-        },
-      ],
+      loadPath: `/lang/{{lng}}.json`,
     },
   })
 
