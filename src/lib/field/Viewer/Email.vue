7469730c0e3a26e309c5870dd9ae5e03d24a8471
Jože Fortun <joze.fortun@gmail.com>
2019-07-17 11:38:14 +0200

    Add Email multi values

--- src/lib/field/Viewer/Email.vue
+++ src/lib/field/Viewer/Email.vue
@@ -1,6 +1,14 @@
 <template>
-  <span v-if="field.options.outputPlain">{{ value }}</span>
-  <span v-else><a :href="'mailto:' + value" target="_blank">{{ value }}</a></span>
+  <div v-if="field.isMulti">
+    <div v-for="(v, index) of value" :key="index">
+      <span v-if="field.options.outputPlain">{{ value[index] }}</span>
+      <span v-else><a :href="'mailto:' + value" target="_blank">{{ value[index] }}</a></span>
+    </div>
+  </div>
+  <div v-else>
+    <span v-if="field.options.outputPlain">{{ value }}</span>
+    <span v-else><a :href="'mailto:' + value" target="_blank">{{ value }}</a></span>
+  </div>
 </template>
 <script>
 import base from './base'
