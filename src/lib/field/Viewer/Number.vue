8349443c49bc2de3e563454e2a933ce8dc5490d2
Jože Fortun <joze.fortun@gmail.com>
2019-07-19 14:47:00 +0200

    Add Number multi viewer

--- src/lib/field/Viewer/Number.vue
+++ src/lib/field/Viewer/Number.vue
@@ -6,6 +6,9 @@ export default {
 
   computed: {
     formatted () {
+      if (this.field.isMulti && this.value) {
+        return this.value.map(v => this.field.options.formatValue(v)).join('\n')
+      }
       return this.field.options.formatValue(this.value)
     },
   },
