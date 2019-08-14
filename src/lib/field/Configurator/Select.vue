631888894768ea8322faf550e1cb9bc2d4d97d80
Jože Fortun <joze.fortun@gmail.com>
2019-08-14 14:46:51 +0200

    Improve multi and select configurator style

--- src/lib/field/Configurator/Select.vue
+++ src/lib/field/Configurator/Select.vue
@@ -1,23 +1,25 @@
 <template>
-  <div class="row no-gutters">
-    <b-form-group class="col-10">
-      <div>{{ $t('field.kind.select.optionsLabel') }}</div>
-      <div v-for="(option, index) in f.options.options" :key="index">
-        <b-form-input plain v-model="f.options.options[index]" size="sm"></b-form-input>
-        <button @click.prevent="f.options.options.splice(index, 1)" class="btn-url">{{ $t('field.kind.select.optionRemove') }}</button>
-      </div>
-      <b-form-input plain v-model="newOption" @keypress.enter.prevent="handleAddOption" size="sm" :placeholder="$t('field.kind.select.optionRemove')"></b-form-input>
-      <button @click.prevent="handleAddOption" :disabled="newOption.length === 0" class="btn-url">+ {{ $t('field.kind.select.optionAdd') }}</button>
-    </b-form-group>
-    <b-form-group>
-      <label class="d-block">{{ $t('field.kind.select.optionType.label') }}</label>
-      <b-form-radio-group
-        v-model="f.options.selectType"
-        :options="selectOptions"
-        stacked
-      ></b-form-radio-group>
-    </b-form-group>
-  </div>
+  <b-row no-gutters>
+    <b-col cols="10">
+      <b-form-group>
+        <div>{{ $t('field.kind.select.optionsLabel') }}</div>
+        <div v-for="(option, index) in f.options.options" :key="index">
+          <b-form-input plain v-model="f.options.options[index]" size="sm"></b-form-input>
+          <button @click.prevent="f.options.options.splice(index, 1)" class="btn-url">{{ $t('field.kind.select.optionRemove') }}</button>
+        </div>
+        <b-form-input plain v-model="newOption" @keypress.enter.prevent="handleAddOption" size="sm" :placeholder="$t('field.kind.select.optionRemove')"></b-form-input>
+        <button @click.prevent="handleAddOption" :disabled="newOption.length === 0" class="btn-url">+ {{ $t('field.kind.select.optionAdd') }}</button>
+      </b-form-group>
+      <b-form-group>
+        <label class="d-block">{{ $t('field.kind.select.optionType.label') }}</label>
+        <b-form-radio-group
+          v-model="f.options.selectType"
+          :options="selectOptions"
+          stacked
+        ></b-form-radio-group>
+      </b-form-group>
+    </b-col>
+  </b-row>
 </template>
 
 <script>
@@ -55,11 +57,3 @@ export default {
   },
 }
 </script>
-
-<style scoped lang="scss">
-input {
-  display: inline;
-  width: calc(100% - 60px);
-  margin-right: 10px;
-}
-</style>
--- src/lib/field/Configurator/multi.vue
+++ src/lib/field/Configurator/multi.vue
@@ -1,13 +1,13 @@
 <template>
   <b-form-group v-if="field.isMulti" class="mt-4">
-    <label class="d-block">{{ $t('field.options.multiDelimiter.label') }}</label>
+    <label class="mt-1">{{ $t('field.options.multiDelimiter.label') }}</label>
     <b-form-radio-group
       v-model="field.options.multiDelimiter"
       :options="selectOptions"
       stacked>
     </b-form-radio-group>
 
-    <label for="custom-delimiter">{{ $t('field.options.multiDelimiter.customLabel') }}</label>
+    <label class="mt-1" for="custom-delimiter">{{ $t('field.options.multiDelimiter.customLabel') }}</label>
     <b-form-input id="custom-delimiter" v-model="field.options.multiDelimiter" :placeholder="$t('field.options.multiDelimiter.customPlaceholder')"></b-form-input>
   </b-form-group>
 </template>
@@ -31,9 +31,3 @@ export default {
   },
 }
 </script>
-
-<style lang="scss" scoped>
-label {
-  margin-top: 10px;
-}
-</style>
