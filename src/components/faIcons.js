309c380ac31cafb1d54b20db0960a99790221b7a
Tomaž Jerman <tomaz.jerman@kendu.si>
2019-08-14 11:51:57 +0200

    Improve icon import logic

---
+++ src/components/faIcons.js
@@ -0,0 +1,27 @@
+import { library } from '@fortawesome/fontawesome-svg-core'
+import { faSort, faSortUp, faSortDown, faWrench, faGripVertical, faDownload, faFileExport, faSearch, faBars, faCloudDownloadAlt } from '@fortawesome/free-solid-svg-icons'
+import { faEye, faFileAlt, faFileWord, faFilePdf, faFilePowerpoint, faFileArchive, faFileExcel, faFileVideo, faFileImage, faEdit, faTrashAlt } from '@fortawesome/free-regular-svg-icons'
+
+library.add(
+  faEye,
+  faSort,
+  faSortUp,
+  faSortDown,
+  faWrench,
+  faGripVertical,
+  faDownload,
+  faFileAlt,
+  faFileWord,
+  faFilePdf,
+  faFilePowerpoint,
+  faFileArchive,
+  faFileExcel,
+  faFileVideo,
+  faFileImage,
+  faFileExport,
+  faEdit,
+  faTrashAlt,
+  faSearch,
+  faBars,
+  faCloudDownloadAlt,
+)
--- src/components/index.js
+++ src/components/index.js
@@ -1,5 +1,6 @@
 import Vue from 'vue'
 import { FontAwesomeIcon } from '@fortawesome/vue-fontawesome'
+import './faIcons'
 
 Vue.component('font-awesome-icon', FontAwesomeIcon)
 Vue.component('permissions-button', () => import('corteza-webapp-common/src/components/Permissions/Button'))
