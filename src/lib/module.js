9873c944541522ad59e2277cb66952807049d9fb
Denis Arh <denis.arh@gmail.com>
2019-08-25 18:56:38 +0200

    Fix system fields

--- src/lib/module.js
+++ src/lib/module.js
@@ -1,6 +1,9 @@
-import BaseModule from 'corteza-webapp-common/src/lib/types/compose/module'
+import BaseModule, { systemFields as baseSystemFields } from 'corteza-webapp-common/src/lib/types/compose/module'
 import { ArrayOf, PropCast } from 'corteza-webapp-common/src/lib/types/common'
 import Field from './field'
+
+// Remap system fields to Field object
+const systemFields = Object.freeze(baseSystemFields.map(f => new Field(f)))
 
 // Keeping this class here for a while (until we move all fields there as well)
 export default class Module extends BaseModule {
@@ -8,5 +11,9 @@ export default class Module extends BaseModule {
     super(m)
     // Override fields from common with the ones we use in compose
     this.fields = PropCast(ArrayOf(Field), m.fields, [])
+  }
+
+  systemFields () {
+    return systemFields
   }
 }
