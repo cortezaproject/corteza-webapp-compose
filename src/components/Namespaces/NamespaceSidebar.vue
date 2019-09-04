43b7b691fe47f8b92e32996b20fa54a702eabd55
Mia Arh <mia@kendu.si>
2019-09-04 18:18:01 +0200

    Apply sidebar styling

--- src/components/Namespaces/NamespaceSidebar.vue
+++ src/components/Namespaces/NamespaceSidebar.vue
@@ -2,32 +2,11 @@
   <aside>
     <b-list-group class="vh-100 bg-white overflow-auto">
       <b-list-group-item class="p-2 sticky-top">
-        <b-form-input placeholder="Search"></b-form-input>
+        <b-form-input placeholder="Search namespaces"></b-form-input>
       </b-list-group-item>
-      <b-list-group-item to="#" class="p-2"><span class="text-truncate d-block" active-class="bg-white text-primary border-transparent">CrasCrasCrasCrasCrasCrasCras justo odio</span></b-list-group-item>
-      <b-list-group-item href="#" class="p-2"><span class="text-truncate d-block">CrasCrasCrasCrasCrasCrasCras justo odio</span></b-list-group-item>
-      <b-list-group-item to="#" class="p-2"><span class="text-truncate d-block">CrasCrasCrasCrasCrasCrasCras justo odio</span></b-list-group-item>
-      <b-list-group-item to="#" class="p-2" active><span class="text-truncate d-block">CrasCrasCrasCrasCrasCrasCras justo odio</span></b-list-group-item>
-      <b-list-group-item to="#" class="p-2"><span class="text-truncate d-block">CrasCrasCrasCrasCrasCrasCras justo odio</span></b-list-group-item>
-      <b-list-group-item to="#" class="p-2"><span class="text-truncate d-block">Porta ac consectetur ac</span></b-list-group-item>
-      <b-list-group-item to="#" class="p-2"><span class="text-truncate d-block">Vestibulum at eros</span></b-list-group-item>
-      <b-list-group-item to="#" class="p-2"><span class="text-truncate d-block">Dapibus ac facilisis in</span></b-list-group-item>
-      <b-list-group-item to="#" class="p-2"><span class="text-truncate d-block">Dapibus ac facilisis in</span></b-list-group-item>
-      <b-list-group-item to="#" class="p-2"><span class="text-truncate d-block">Porta ac consectetur ac</span></b-list-group-item>
-      <b-list-group-item to="#" class="p-2"><span class="text-truncate d-block">Vestibulum at eros</span></b-list-group-item>
-      <b-list-group-item to="#" class="p-2"><span class="text-truncate d-block">Dapibus ac facilisis in</span></b-list-group-item>
-      <b-list-group-item to="#" class="p-2"><span class="text-truncate d-block">Morbi leo risus Morbi leo risusMorbi leo risus</span></b-list-group-item>
-      <b-list-group-item to="#" class="p-2"><span class="text-truncate d-block">Dapibus ac facilisis in</span></b-list-group-item>
-      <b-list-group-item to="#" class="p-2"><span class="text-truncate d-block">Cras justo odio</span></b-list-group-item>
-      <b-list-group-item to="#" class="p-2"><span class="text-truncate d-block">Dapibus ac facilisis in</span></b-list-group-item>
-      <b-list-group-item to="#" class="p-2"><span class="text-truncate d-block">Dapibus ac facilisis in</span></b-list-group-item>
-      <b-list-group-item to="#" class="p-2"><span class="text-truncate d-block">Porta ac consectetur ac</span></b-list-group-item>
-      <b-list-group-item to="#" class="p-2"><span class="text-truncate d-block">Vestibulum at eros</span></b-list-group-item>
-      <b-list-group-item to="#" class="p-2"><span class="text-truncate d-block">Dapibus ac facilisis in</span></b-list-group-item>
-      <b-list-group-item to="#" class="p-2"><span class="text-truncate d-block">Morbi leo risus Morbi leo risusMorbi leo risus</span></b-list-group-item>
-      <b-list-group-item to="#" class="p-2"><span class="text-truncate d-block">Dapibus ac facilisis in</span></b-list-group-item>
-      <b-list-group-item to="#" class="p-2"><span class="text-truncate d-block">Cras justo odio</span></b-list-group-item>
-      <b-list-group-item to="#" class="p-2"><span class="text-truncate d-block">Dapibus ac facilisis in</span></b-list-group-item>
+      <b-list-group-item to="1" class="p-2" active-class="active text-primary border-top-0 border-bottom-0"><span class="text-truncate d-block" >CrasCrasCrasCrasCrasCrasCras justo odio</span></b-list-group-item>
+      <b-list-group-item to="2" class="p-2" active-class="active text-primary border-top-0 border-bottom-0"><span class="text-truncate d-block" >CrasCrasCrasCrasCrasCrasCras justo odio</span></b-list-group-item>
+      <b-list-group-item to="3" class="p-2" active-class="active text-primary border-top-0 border-bottom-0"><span class="text-truncate d-block" >CrasCrasCrasCrasCrasCrasCras justo odio</span></b-list-group-item>
     </b-list-group>
   </aside>
 </template>
@@ -37,8 +16,9 @@
     width: 200px;
 
     .list-group-item {
-      .active {
+      &.active {
         border-right: 3px solid $primary;
+        background-color: rgba($primary, 0.15);
       }
     }
   }
