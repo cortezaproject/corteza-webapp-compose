1a003540beb4549dc0f9892fb7b254a0092efe49
Tomaž Jerman <tomaz.jerman@kendu.si>
2019-10-07 10:31:07 +0200

    Tweak record list's record fetching

--- tests/unit/specs/lib/block/View/recordList.spec.js
+++ tests/unit/specs/lib/block/View/recordList.spec.js
@@ -6,6 +6,7 @@ import RecordList from 'corteza-webapp-compose/src/lib/block/View/RecordList'
 import Module from 'corteza-webapp-compose/src/lib/module'
 import { shallowMount } from 'corteza-webapp-compose/tests/lib/helpers'
 import sinon from 'sinon'
+import fp from 'flush-promises'
 
 const records = [
   {recordID: '100', moduleID: '200', values: [{ name: 'n1', value: 'v1'},{ name: "n2", value: 'v2'}], namespaceID: '300' },
@@ -22,12 +23,15 @@ let mod = new Module({
 
 
 describe('lib/block/View/RecordList', () => {
-  let propsData, $ComposeAPI
+  let propsData, $ComposeAPI, $auth
   beforeEach(() => {
     propsData = {
       options: {},
       page: {},
       namespace: {},
+    }
+    $auth = {
+      user: { userID: '888' },
     }
     $ComposeAPI = {
       recordList: sinon.mock().resolves({ set: [], filter: {} })
@@ -38,14 +42,14 @@ describe('lib/block/View/RecordList', () => {
   })
 
   const mountRL = (opt) => shallowMount(RecordList, {
-    mocks: { $ComposeAPI },
+    mocks: { $ComposeAPI, $auth },
     propsData,
     ...opt,
   })
 
   describe('records', () => {
     beforeEach(() => {
-      sinon.stub(RecordList, 'beforeMount')
+      sinon.stub(RecordList, 'created')
     })
 
     it('Don\'t render any records if module is not defined', () => {
@@ -64,5 +68,53 @@ describe('lib/block/View/RecordList', () => {
 
       expect(RecordList.computed.records.call(local)).to.have.length(2)
     })
+  })
+
+  describe('record fetching', () => {
+    beforeEach(() => {
+      sinon.stub(RecordList.computed, 'recordListModule').returns(new Module({ moduleID: '333' }))
+      propsData.options.pageID = '111'
+      propsData.options.moduleID = '333'
+    })
+
+    const rspFilter = () => ({
+      count: 2,
+      page: 1,
+      perPage: 20,
+      sort: '',
+      filter: '',
+    })
+
+    it('on init -- prepare initial filter', () => {
+      sinon.stub(RecordList.methods, 'updateRecordList')
+      const wrap = mountRL()
+
+      expect(wrap.vm.filter).to.not.deep.eq({})
+    })
+
+    it('on init -- evaluate prefilter', () => {
+      sinon.stub(RecordList.methods, 'updateRecordList')
+      propsData.options.prefilter = '${userID} is awesome'
+      const wrap = mountRL()
+
+      expect(wrap.vm.filter.filter).to.include($auth.user.userID)
+    })
+
+    it('fetch records with initial query', async () => {
+      $ComposeAPI.recordList = sinon.mock().resolves({ set: [], filter: rspFilter() })
+      const wrap = mountRL()
+      await fp()
+
+      sinon.assert.calledOnce($ComposeAPI.recordList)
+      expect(wrap.vm.filter).to.deep.eq(rspFilter())
+    })
+  })
+
+  // @todo...
+  describe('record filtering', () => {
+    it('fallback to prefilter on empty query')
+    it('make numeric query')
+    it('make string query -- convert wildcards to sql wildcards')
+    it('join field queries')
   })
 })
