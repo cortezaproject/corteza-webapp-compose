242bd2320e981b00eb40475939cc8a2387c44ec4
Jože Fortun <joze.fortun@gmail.com>
2019-10-24 14:51:34 +0200

    Add check to users

--- src/mixins/users.js
+++ src/mixins/users.js
@@ -4,15 +4,17 @@ export default {
       let userIDs = new Set()
       fields.filter(c => c.kind === 'User').forEach(u => {
         records.forEach(r => {
-          const userID = r.values[u.name]
-          if (u.isMulti) {
-            userID.forEach(uID => {
-              if (uID && !this.$store.getters['user/findByID'](uID)) {
-                userIDs.add(uID)
-              }
-            })
-          } else if (userID && !this.$store.getters['user/findByID'](userID)) {
-            userIDs.add(userID)
+          if (r) {
+            const userID = r.values[u.name]
+            if (u.isMulti) {
+              userID.forEach(uID => {
+                if (uID && !this.$store.getters['user/findByID'](uID)) {
+                  userIDs.add(uID)
+                }
+              })
+            } else if (userID && !this.$store.getters['user/findByID'](userID)) {
+              userIDs.add(userID)
+            }
           }
         })
       })
