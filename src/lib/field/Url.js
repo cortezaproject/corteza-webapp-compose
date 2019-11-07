594a6bd223867227ce599c35b69c8be23cf88861
Tomaž Jerman <tomaz.jerman@kendu.si>
2019-11-07 11:55:43 +0100

    Fix lib/field/url trimmers & construct with URL

--- src/lib/field/Url.js
+++ src/lib/field/Url.js
@@ -27,36 +27,74 @@ export class Url {
   }
 }
 
+/**
+ * Processes a given URL and returns a URL object
+ * @param {String} url URL to process
+ * @returns {URL}
+ */
+function makeURL (url) {
+  if (!(/^http(s)?:\/\//.test(url))) {
+    url = 'https://' + url
+  }
+  return new URL(url)
+}
+
+/**
+ * Removes URL's fragment
+ * @param {String} url URL to process
+ * @returns {String}
+ */
 export function trimUrlFragment (url) {
-  if (url && url.length > 4) {
-    return url.split('#')[0]
+  if (!url) {
+    return url
   }
-  return url
+
+  url = makeURL(url)
+  url.hash = ''
+  return url.toString()
 }
 
+/**
+ * Removes URL's query
+ * @param {String} url URL to process
+ * @returns {String}
+ */
 export function trimUrlQuery (url) {
-  if (url && url.length > 4) {
-    return url.split('?')[0]
+  if (!url) {
+    return url
   }
-  return url
+
+  url = makeURL(url)
+  url.search = ''
+  return url.toString()
 }
 
+/**
+ * Removes URL's path
+ * @param {String} url URL to process
+ * @returns {String}
+ */
 export function trimUrlPath (url) {
-  if (url && url.length > 4) {
-    var urlComponents = url.split('/')
-
-    return urlComponents[0] + '//' + urlComponents[2]
+  if (!url) {
+    return url
   }
-  return url
+
+  url = makeURL(url)
+  url.pathname = ''
+  return url.toString()
 }
 
+/**
+ * Assures that the given URL will provide a https schema
+ * @param {String} url URL to process
+ * @returns {String}
+ */
 export function onlySecureUrl (url) {
-  if (url && url.length > 4) {
-    if (url.charAt(4).toLowerCase() === 's') {
-      return url
-    }
-
-    return url.slice(0, 4) + 's' + url.slice(4)
+  if (!url) {
+    return url
   }
-  return url
+
+  url = makeURL(url)
+  url.protocol = 'https'
+  return url.toString()
 }
--- tests/unit/specs/lib/field/Url.spec.js
+++ tests/unit/specs/lib/field/Url.spec.js
@@ -4,79 +4,174 @@
 import { expect } from 'chai'
 import { trimUrlFragment, trimUrlQuery, trimUrlPath, onlySecureUrl } from 'corteza-webapp-compose/src/lib/field/Url'
 
-var should = require('chai').should()
-
 describe('lib/field/Url.js', () => {
-  // Test trimUrlFragment
-  it('trimUrlFragment - Should remove # from the url', function () {
-    var result = trimUrlFragment('http://example.tld/basic.html#testAnchor')
-    result.should.equal('http://example.tld/basic.html')
-  })
-  it('trimUrlFragment - Should not change the url', function () {
-    var result = trimUrlFragment('https://example.tld')
-    result.should.equal('https://example.tld')
-  })
-  it('trimUrlFragment - Should not remove the query', function () {
-    var result = trimUrlFragment('http://example.tld/basic.html?id=123')
-    result.should.equal('http://example.tld/basic.html?id=123')
-  })
-  it('trimUrlFragment - Should not remove the extra path from the url', function () {
-    var result = trimUrlFragment('http://example.tld/basic/index.html')
-    result.should.equal('http://example.tld/basic/index.html')
+  it('trim URL fragment', () => {
+    const tests = [
+      {
+        name: 'invalid value - undefined',
+        url: undefined,
+        expected: undefined,
+      },
+      {
+        name: 'invalid value - empty string',
+        url: '',
+        expected: '',
+      },
+      {
+        name: 'partial url - missing schema',
+        url: 'example.tld/',
+        expected: 'https://example.tld/',
+      },
+      {
+        name: 'remove fragment',
+        url: 'http://example.tld/basic/index.html?q1=v1#testAnchor',
+        expected: 'http://example.tld/basic/index.html?q1=v1',
+      },
+
+      {
+        name: 'leave as is, if no fragment provided',
+        url: 'https://example.tld/',
+        expected: 'https://example.tld/',
+      },
+
+      {
+        name: 'leave as is, if no fragment provided - with path & query',
+        url: 'http://example.tld/basic/index.html?q1=v1',
+        expected: 'http://example.tld/basic/index.html?q1=v1',
+      },
+    ]
+
+    for (const t of tests) {
+      expect(trimUrlFragment(t.url), t.name).to.eq(t.expected)
+    }
   })
 
-  // Test trimUrlQuery
-  it('trimUrlQuery - Should not remove # from the url', function () {
-    var result = trimUrlQuery('http://example.tld/basic.html#testAnchor')
-    result.should.equal('http://example.tld/basic.html#testAnchor')
-  })
-  it('trimUrlQuery - Should not change the url', function () {
-    var result = trimUrlQuery('https://example.tld')
-    result.should.equal('https://example.tld')
-  })
-  it('trimUrlQuery - Should remove the query', function () {
-    var result = trimUrlQuery('http://example.tld/basic.html?id=123')
-    result.should.equal('http://example.tld/basic.html')
-  })
-  it('trimUrlQuery - Should not remove the extra path from the url', function () {
-    var result = trimUrlQuery('http://example.tld/basic/index.html')
-    result.should.equal('http://example.tld/basic/index.html')
+  it('trimUrlQuery', () => {
+    const tests = [
+      {
+        name: 'invalid value - undefined',
+        url: undefined,
+        expected: undefined,
+      },
+      {
+        name: 'invalid value - empty string',
+        url: '',
+        expected: '',
+      },
+      {
+        name: 'partial url - missing schema',
+        url: 'example.tld/',
+        expected: 'https://example.tld/',
+      },
+      {
+        name: 'remove query',
+        url: 'http://example.tld/basic/index.html?q1=v1#testAnchor',
+        expected: 'http://example.tld/basic/index.html#testAnchor',
+      },
+
+      {
+        name: 'remove multiple query params',
+        url: 'http://example.tld/basic/index.html?q1=v1&q2=v2&q3=v3#testAnchor',
+        expected: 'http://example.tld/basic/index.html#testAnchor',
+      },
+
+      {
+        name: 'leave as is, if no query provided',
+        url: 'https://example.tld/',
+        expected: 'https://example.tld/',
+      },
+
+      {
+        name: 'leave as is, if no query provided - with path & hash',
+        url: 'http://example.tld/basic/index.html#testAnchor',
+        expected: 'http://example.tld/basic/index.html#testAnchor',
+      },
+    ]
+
+    for (const t of tests) {
+      expect(trimUrlQuery(t.url), t.name).to.eq(t.expected)
+    }
   })
 
-  // Test trimUrlPath
-  it('trimUrlPath - Should remove # from the url', function () {
-    var result = trimUrlPath('http://example.tld/basic.html#testAnchor')
-    result.should.equal('http://example.tld')
-  })
-  it('trimUrlPath - Should not change the url', function () {
-    var result = trimUrlPath('https://example.tld')
-    result.should.equal('https://example.tld')
-  })
-  it('trimUrlPath - Should remove the query', function () {
-    var result = trimUrlPath('http://example.tld/basic.html?id=123')
-    result.should.equal('http://example.tld')
-  })
-  it('trimUrlPath - Should remove the extra path from the url', function () {
-    var result = trimUrlPath('http://example.tld/basic/index.html')
-    result.should.equal('http://example.tld')
+  it('trimUrlPath', () => {
+    const tests = [
+      {
+        name: 'invalid value - undefined',
+        url: undefined,
+        expected: undefined,
+      },
+      {
+        name: 'invalid value - empty string',
+        url: '',
+        expected: '',
+      },
+      {
+        name: 'partial url - missing schema',
+        url: 'example.tld/',
+        expected: 'https://example.tld/',
+      },
+      {
+        name: 'remove path',
+        url: 'http://example.tld/basic/index.html?q1=v1#testAnchor',
+        expected: 'http://example.tld/?q1=v1#testAnchor',
+      },
+
+      {
+        name: 'leave as is, if no path provided',
+        url: 'https://example.tld/',
+        expected: 'https://example.tld/',
+      },
+
+      {
+        name: 'leave as is, if no path provided - with path & hash',
+        url: 'http://example.tld/?q1=v1#testAnchor',
+        expected: 'http://example.tld/?q1=v1#testAnchor',
+      },
+    ]
+
+    for (const t of tests) {
+      expect(trimUrlPath(t.url), t.name).to.eq(t.expected)
+    }
   })
 
-  // Test onlySecureUrl
-  it('onlySecureUrl - Should not remove # from the url and turn it secure', function () {
-    var result = onlySecureUrl('http://example.tld/basic.html#testAnchor')
-    result.should.equal('https://example.tld/basic.html#testAnchor')
-  })
-  it('onlySecureUrl - Should not change the url', function () {
-    var result = onlySecureUrl('https://example.tld')
-    result.should.equal('https://example.tld')
-  })
-  it('onlySecureUrl - Should not remove the query and turn it secure', function () {
-    var result = onlySecureUrl('http://example.tld/basic.html?id=123')
-    result.should.equal('https://example.tld/basic.html?id=123')
-  })
-  it('onlySecureUrl - Should not remove the extra path from the url and turn it secure', function () {
-    var result = onlySecureUrl('http://example.tld/basic/index.html')
-    result.should.equal('https://example.tld/basic/index.html')
-  })
+  it('onlySecureUrl', () => {
+    const tests = [
+      {
+        name: 'invalid value - undefined',
+        url: undefined,
+        expected: undefined,
+      },
+      {
+        name: 'invalid value - empty string',
+        url: '',
+        expected: '',
+      },
+      {
+        name: 'partial url - missing schema',
+        url: 'example.tld/',
+        expected: 'https://example.tld/',
+      },
+      {
+        name: 'securify',
+        url: 'http://example.tld/basic/index.html?q1=v1#testAnchor',
+        expected: 'https://example.tld/basic/index.html?q1=v1#testAnchor',
+      },
 
+      {
+        name: 'leave as is if secure',
+        url: 'https://example.tld/',
+        expected: 'https://example.tld/',
+      },
+
+      {
+        name: 'leave as is, if secure - with path, query & hash',
+        url: 'https://example.tld/basic/index.html?q1=v1#testAnchor',
+        expected: 'https://example.tld/basic/index.html?q1=v1#testAnchor',
+      },
+    ]
+
+    for (const t of tests) {
+      expect(onlySecureUrl(t.url), t.name).to.eq(t.expected)
+    }
+  })
 })
