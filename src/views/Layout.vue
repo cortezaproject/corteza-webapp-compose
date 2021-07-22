6c91cf8578c9478b74eed029bbf6493a7be8d6ad
Mario Burazer <mario.burazer@crust.tech>
2021-07-22 14:11:34 +0200

    Move sidebar close event

--- src/views/Layout.vue
+++ src/views/Layout.vue
@@ -75,6 +75,18 @@ export default {
       pinned: false,
     }
   },
+
+  created () {
+    this.$root.$on('close-sidebar', () => {
+      this.expanded = false
+      this.pinned = false
+    })
+  },
+
+  beforeDestroy () {
+    this.$root.$off('close-sidebar')
+  },
+
 }
 </script>
 
