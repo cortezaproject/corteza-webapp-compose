78924b8d1fc32af3e909de659b51a98382dc66ba
Jože Fortun <joze.fortun@gmail.com>
2021-12-10 16:31:14 +0100

    WIP

---
+++ src/components/ModuleFields/Viewer/Geometry.vue
@@ -0,0 +1,34 @@
+<template>
+  <div
+    v-if="formatted"
+  >
+    <p>
+      {{ coordinates[0] }}, {{ coordinates[1] }}
+    </p>
+
+    <errors :errors="errors" />
+  </div>
+</template>
+<script>
+import base from './base'
+
+export default {
+  extends: base,
+
+  computed: {
+    formatted () {
+      return JSON.parse(this.value || '{}')
+    },
+
+    coordinates () {
+      return this.formatted.coordinates || ['0', '0']
+    },
+  },
+}
+</script>
+
+<style>
+.multiline {
+  white-space: pre-line;
+}
+</style>
