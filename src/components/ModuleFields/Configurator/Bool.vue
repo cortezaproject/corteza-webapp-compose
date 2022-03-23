bac65e6bd3b675a8677c13673822153377b1d071
Tomaž Jerman <tomaz.jerman121@gmail.com>
2022-03-23 16:18:31 +0100

    Add front-end support for boolean label res. translations

--- src/components/ModuleFields/Configurator/Bool.vue
+++ src/components/ModuleFields/Configurator/Bool.vue
@@ -2,29 +2,67 @@
   <div>
     <div>
       <label class="d-block">{{ $t('kind.bool.checkedValueLabel') }}</label>
-      <b-form-input
-        v-model="f.options.trueLabel"
-        :placeholder="$t('kind.bool.checkedValuePlaceholder')"
-      />
+      <b-input-group>
+        <b-form-input
+          v-model="f.options.trueLabel"
+          :placeholder="$t('kind.bool.checkedValuePlaceholder')"
+        />
+        <b-input-group-append>
+          <field-bool-translator
+            v-if="field"
+            :field="field"
+            :module="module"
+            :highlight-key="`meta.true.label`"
+            size="sm"
+            :disabled="isNew"
+            button-variant="light"
+          />
+        </b-input-group-append>
+      </b-input-group>
     </div>
     <div>
       <label class="d-block">{{ $t('kind.bool.uncheckedValueLabel') }}</label>
-      <b-form-input
-        v-model="f.options.falseLabel"
-        :placeholder="$t('kind.bool.uncheckedValuePlaceholder')"
-      />
+      <b-input-group>
+        <b-form-input
+          v-model="f.options.falseLabel"
+          :placeholder="$t('kind.bool.uncheckedValuePlaceholder')"
+        />
+        <b-input-group-append>
+          <field-bool-translator
+            v-if="field"
+            :field="field"
+            :module="module"
+            :highlight-key="`meta.false.label`"
+            size="sm"
+            :disabled="isNew"
+            button-variant="light"
+          />
+        </b-input-group-append>
+      </b-input-group>
     </div>
   </div>
 </template>
 
 <script>
 import base from './base'
+import { NoID } from '@cortezaproject/corteza-js'
+import FieldBoolTranslator from 'corteza-webapp-compose/src/components/Admin/Module/FieldBoolTranslator'
 
 export default {
   i18nOptions: {
     namespaces: 'field',
   },
 
+  components: {
+    FieldBoolTranslator,
+  },
+
   extends: base,
+
+  computed: {
+    isNew () {
+      return this.module.moduleID === NoID || this.field.fieldID === NoID
+    },
+  },
 }
 </script>
