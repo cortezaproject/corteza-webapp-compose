b7ab3e47562183fc079e2926f719a3c660c680ab
nzidol <nzidol@users.noreply.github.com>
2022-04-06 14:48:30 +1200

    For field type geometry: - add configuration tab for initial zoom-level (inital value 3) for button "open map" : - in editor centered on default value at configured zoom-level. - in viewer centered on first marker at configured zoom-level

---
+++ src/components/ModuleFields/Configurator/Geometry.vue
@@ -0,0 +1,26 @@
+<template>
+  <div>
+    <b-form-group>
+      <label class="d-block">Initial Zoom Level</label>
+      <b-form-input
+        v-model="f.options.zoomLevel"
+        placeholder="Zoom Level"
+        type="number"
+        min="0"
+        max="18"
+      />
+    </b-form-group>
+  </div>
+</template>
+
+<script>
+import base from './base'
+
+export default {
+  i18nOptions: {
+    namespaces: 'field',
+  },
+
+  extends: base,
+}
+</script>
