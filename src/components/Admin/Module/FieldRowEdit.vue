8378e73ce23125fa47c45a558f09de9babd691d2
Jože Fortun <joze.fortun@gmail.com>
2022-06-07 15:24:53 +0200

    Sort field kinds in module field edit

--- src/components/Admin/Module/FieldRowEdit.vue
+++ src/components/Admin/Module/FieldRowEdit.vue
@@ -185,7 +185,7 @@ export default {
         // for now this field is hidden, since it's implementation is mia.
         .map(kind => {
           return { kind, label: this.$t('fieldKinds.' + kind + '.label') }
-        })
+        }).sort((a, b) => a.label.localeCompare(b.text))
     },
 
     exists () {
